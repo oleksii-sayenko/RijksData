00e40304a426ba7a958b93d5cc3fbac05e81084f
Olekseii Sayenko <aleksey_sayenko@epam.com>
2024-08-27 13:14:52 +0300

    Initial Commit

---
+++ RijksData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RijksData
+//
+//  Created by Aleksey Sayenko on 27.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
